e4d14eb73544f6df2ad8ccb17496d7062f8c3a47
Ishaan Bhagat <ishaanbhagat@princeton.edu>
2024-06-20 15:16:37 +0300

    Axios library now working

--- testFetch.js
+++ testFetch.js
@@ -1,14 +1,17 @@
-const testFetch = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:8000/patient_data/1');
-      if (!response.ok) {
-        throw new Error(`Error fetching patient data: ${response.statusText}`);
-      }
-      const data = await response.json();
-      console.log('Patient Data:', data);
-    } catch (error) {
-      console.error('Error fetching patient data:', error);
-    }
-  };
-  
-  testFetch();
+const axios = require('axios');
+
+const fetchPatientData = async () => {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/patient_data/2', {
+      proxy: {
+        host: '127.0.0.1',
+        port: 8000,
+      },
+    });
+    console.log('Patient Data:', response.data);
+  } catch (error) {
+    console.error('Error fetching patient data:', error);
+  }
+};
+
+fetchPatientData();
